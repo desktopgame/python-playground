e8ba6b304ebcded096bfaa4cb6f7ec024de05f36
desktopgame <>
2025-04-14 20:30:53 +0900

    Update: change message

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,7 @@ function LoadingScreen() {
           className="h-52 w-52 animate-pulse"
         />
         <p className="sm:text-1xl mt-2 animate-pulse whitespace-pre-wrap text-center text-xl font-semibold tracking-tighter text-white">
-          Loading WebAssembly
+          Loading...
         </p>
       </div>
     </section>
